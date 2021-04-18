425443e85c55200841d99f82b07a29de59356344
Sooraj Sannabhadti <2005sooraj@gmail.com>
2021-04-18 22:51:26 +0800

    Added Countless New Features
    Too many features to actually list here. Go to the documentation page to see them all. Also removed some files and created example files of how they are saved.

--- Custom Text Editor Window Edition.py
+++ Custom Text Editor Window Edition.py
@@ -12,6 +12,7 @@ from tkinter import *
 # from tkinter.messagebox import showerror
 from tkinter import filedialog
 from tkinter import messagebox
+from tkinter import colorchooser
 import webbrowser
 
 
@@ -19,11 +20,11 @@ import webbrowser
 
 
 save_location = ""
-font_size = 15
+font_size = 11
 font_type = "Arial"
-bold_or_not = "bold"
 max_font_size = 100
 min_font_size = 3
+used_tags = []
 
 root = Tk("Text Editor")
 root.geometry("500x500")
@@ -31,10 +32,10 @@ root.wm_title("Custom Text Editor Interface Edition")
 
 title = StringVar()
 title.set("")
-title_of_file = Label(textvariable=title, font=(font_type, font_size, bold_or_not))
+title_of_file = Label(textvariable=title, font=("Arial", 15, "bold"))
 title_of_file.pack(side=TOP, fill=X)
 
-textbox = Text(root, state=NORMAL)
+textbox = Text(root, state=NORMAL, font=(font_type, font_size, "normal"))
 
 scroll_bar_vertical = Scrollbar(root, orient=VERTICAL)
 scroll_bar_vertical.pack(side=RIGHT, fill=Y)
@@ -75,7 +76,7 @@ def quit_app(Event=None):
 def save_file(Event=None):
     global textbox
     if save_location != "":
-        text_to_save = textbox.get("1.0", "end-1c")
+        text_to_save = textbox.get("1.0", END)
         file1 = open(save_location, "w+")
         file1.write(text_to_save)
         file1.close()
@@ -86,8 +87,8 @@ def save_file(Event=None):
 
 def save_as_file(Event=None):
     global textbox
-    text_to_save_as = textbox.get("1.0", "end-1c")
-    files = [("Custom Text Editor Window Edition Files", "*.ctewe"),
+    text_to_save_as = textbox.get("1.0", END)
+    files = [("Custom Text Editor Interface Edition Files", "*.cteie"),
              ("Text Document", "*.txt"),
              ("Python Files", "*.py"),
              ("HTML Files", "*.html"),
@@ -103,7 +104,13 @@ def save_as_file(Event=None):
 def open_file(Event=None):
     try:
         global save_location
-        save_location = filedialog.askopenfilename(title = "Select file",filetypes = (("All Files","*.*"),("Text Files","*.txt"),("Python Files","*.py")))
+        files = [("Custom Text Editor Interface Edition Files", "*.cteie"),
+                 ("Text Document", "*.txt"),
+                 ("Python Files", "*.py"),
+                 ("HTML Files", "*.html"),
+                 ("Cascading Style Sheets Files", "*.css"),
+                 ("All Files", "*.*")]
+        save_location = filedialog.askopenfilename(title="Select file",filetypes=files)
         if save_location != "":
             infile = open(save_location,"r")
             textbox.config(state=NORMAL)
@@ -113,8 +120,8 @@ def open_file(Event=None):
             infile.close()
         get_file_name()
         print("Opened file for editing.")
-    except Exception as e:
-        messagebox.showerror("Exception",e)
+    except Exception:
+        messagebox.showerror("Error Opening File", "File format is not supported.")
 
 def new_file(Event=None):
     if len(textbox.get("1.0", END)) != 1:
@@ -155,10 +162,10 @@ def increase_font(Event=None):
         messagebox.showerror("Error", "Font size cannot go higher than 100. Please change the max_font_size variable to increase the limit.")
     else:
         font_size = font_size + 1
-        textbox.config(font=(font_type, font_size, bold_or_not))
+        textbox.config(font=(font_type, font_size))
 
-    textbox.tag_add("start", "2.8", END)
-    textbox.tag_config("start", background="lime green", foreground="red", font=("Helvetica", 50, "bold"))
+    # textbox.tag_add("start", "1.0", END)
+    # textbox.tag_config("start", background="lime green", font=("Helvetica", 50, "italic"))
 
 def decrease_font(Event=None):
     global font_size
@@ -166,13 +173,35 @@ def decrease_font(Event=None):
         messagebox.showerror("Error", "Font size cannot go lower than 3. Please change the min_font_size variable to decrease the limit.")
     else:
         font_size = font_size - 1
-        textbox.config(font=(font_type, font_size, bold_or_not))
+        textbox.config(font=(font_type, font_size))
+
+def specific_size_font(Event=None):
+    print()
+
+def change_text_colour():
+    colour_code = colorchooser.askcolor(title ="Choose a colour")
+    colour_code = colour_code[-1]
+    start_selection = textbox.index("sel.first")
+    end_selection = textbox.index("sel.last")
+    not_valid = True
+    i = 0
+    while not_valid == True:
+        if len(used_tags) == 0:
+            not_valid = False
+        elif used_tags[-1] >= i:
+            i = i + 1
+        else:
+            not_valid = False
+    current_tag = f"selection{i}"
+    textbox.tag_add(current_tag, start_selection, end_selection)
+    textbox.tag_config(current_tag, foreground=colour_code)
+    used_tags.append(i)
 
 def documentation(Event=None):
     webbrowser.open_new("https://github.com/WhenLifeHandsYouLemons/Custom-Text-Editor/wiki/Documentation")
 
 def about_menu(Event=None):
-    messagebox.showinfo("About Custom Text Editor Windows Edition","A custom text editor made for a fun side project. Comes with a terminal version and an encryptor which adds an extra layer of security by asking for a password before showing the text editor.\n\nCreated using the Python programming language and the Tkinter module.")
+    messagebox.showinfo("About Custom Text Editor Interface Edition","A custom text editor made for a fun side project. Comes with a terminal version and an encryptor which adds an extra layer of security by asking for a password before showing the text editor.\n\nCreated by Sooraj.S")
 
 def copy_event(Event=None):
     textbox.event_generate("<<Copy>>")
@@ -184,11 +213,10 @@ def cut_event(Event=None):
     textbox.event_generate("<<Cut>>")
 
 def undo_event(Event=None):
-    textbox.get("", END)
-    textbox.delete("1.0", END)
+    print("")
 
 def redo_event(Event=None):
-    textbox.event_generate("<<Redo>>")
+    print("")
 
 def select_all(Event=None):
     textbox.event_generate("<<SelectAll>>")
@@ -196,18 +224,74 @@ def select_all(Event=None):
 def deselect_all(Event=None):
     textbox.event_generate("<<SelectNone>>")
 
+def normal_text_style(Event=None):
+    start_selection = textbox.index("sel.first")
+    end_selection = textbox.index("sel.last")
+    not_valid = True
+    i = 0
+    while not_valid == True:
+        if len(used_tags) == 0:
+            not_valid = False
+        elif used_tags[-1] >= i:
+            i = i + 1
+        else:
+            not_valid = False
+    current_tag = f"selection{i}"
+    textbox.tag_add(current_tag, start_selection, end_selection)
+    textbox.tag_config(current_tag, font=(font_type, font_size, "normal"))
+    used_tags.append(i)
+
+def bold_text_style(Event=None):
+    start_selection = textbox.index("sel.first")
+    end_selection = textbox.index("sel.last")
+    not_valid = True
+    i = 0
+    while not_valid == True:
+        if len(used_tags) == 0:
+            not_valid = False
+        elif used_tags[-1] >= i:
+            i = i + 1
+        else:
+            not_valid = False
+    current_tag = f"selection{i}"
+    textbox.tag_add(current_tag, start_selection, end_selection)
+    textbox.tag_config(current_tag, font=(font_type, font_size, "bold"))
+    used_tags.append(i)
+
+def italic_text_style(Event=None):
+    start_selection = textbox.index("sel.first")
+    end_selection = textbox.index("sel.last")
+    not_valid = True
+    i = 0
+    while not_valid == True:
+        if len(used_tags) == 0:
+            not_valid = False
+        elif used_tags[-1] >= i:
+            i = i + 1
+        else:
+            not_valid = False
+    current_tag = f"selection{i}"
+    textbox.tag_add(current_tag, start_selection, end_selection)
+    textbox.tag_config(current_tag, font=(font_type, font_size, "italic"))
+    used_tags.append(i)
+
 menubar = Menu(root)
 filemenu = Menu(menubar, tearoff=0)
 filemenu.add_command(label="New File", accelerator="Ctrl+N", command=new_file)
 root.bind_all("<Control-n>", new_file)
 filemenu.add_command(label="Open...", accelerator="Ctrl+O", command=open_file)
 root.bind_all("<Control-o>", open_file)
+filemenu.add_command(label="View File", command=view_file)
+filemenu.add_separator()
 filemenu.add_command(label="Save", accelerator="Ctrl+S", command=save_file)
 root.bind_all("<Control-s>", save_file)
 filemenu.add_command(label="Save As...", accelerator="Alt+S", command=save_as_file)
 root.bind_all("<Alt-s>", save_as_file)
-filemenu.add_command(label="View", accelerator="Ctrl+Shift+V", command=view_file)
-root.bind_all("<Control-Shift-v>", view_file)
+filemenu.add_separator()
+filemenu.add_command(label="Edit Mode",  accelerator="Alt+E", command=edit_mode)
+root.bind_all("<Alt-e>", edit_mode)
+filemenu.add_command(label="View Mode", accelerator="Alt+V", command=view_mode)
+root.bind_all("<Alt-v>", view_mode)
 filemenu.add_separator()
 filemenu.add_command(label="Exit", accelerator="Ctrl+W", command=quit_app)
 root.bind_all("<Control-w>", quit_app)
@@ -218,35 +302,50 @@ editmenu.add_command(label="Undo", accelerator="Ctrl+Z", command=undo_event)
 root.bind_all("<Control-z>", undo_event)
 editmenu.add_command(label="Redo", accelerator="Ctrl+Y", command=redo_event)
 root.bind_all("<Control-y>", redo_event)
+editmenu.add_separator()
 editmenu.add_command(label="Cut", accelerator="Ctrl+X", command=cut_event)
 root.bind_all("<Control-x>", cut_event)
 editmenu.add_command(label="Copy",  accelerator="Ctrl+C", command=copy_event)
 root.bind_all("<Control-c>", copy_event)
 editmenu.add_command(label="Paste", accelerator="Ctrl+V", command=paste_event)
 root.bind_all("<Control-v>", paste_event)
+editmenu.add_separator()
 editmenu.add_command(label="Select All", accelerator="Ctrl+A", command=select_all)
 root.bind_all("<Control-a>", select_all)
-editmenu.add_command(label="Deselect All", accelerator="Ctrl+Shift+A", command=deselect_all)
-root.bind_all("<Control-Shift-a>", deselect_all)
+editmenu.add_command(label="Deselect All", accelerator="Alt+A", command=deselect_all)
+root.bind_all("<Alt-a>", deselect_all)
 editmenu.add_separator()
-editmenu.add_command(label="Edit Mode",  accelerator="Alt+E", command=edit_mode)
-root.bind_all("<Alt-e>", edit_mode)
-editmenu.add_command(label="View Mode", accelerator="Alt+V", command=view_mode)
-root.bind_all("<Alt-v>", view_mode)
-editmenu.add_separator()
-editmenu.add_command(label="Change Font",  accelerator="Alt+F", command=edit_mode)
-root.bind_all("<Alt-f>", edit_mode)
-editmenu.add_command(label="Increase Font", accelerator="Alt+I", command=increase_font)
-root.bind_all("<Alt-i>", increase_font)
-editmenu.add_command(label="Decrease Font", accelerator="Alt+D", command=decrease_font)
-root.bind_all("<Alt-d>", decrease_font)
+editmenu.add_command(label="Edit Preferences")
 menubar.add_cascade(label="Edit", menu=editmenu)
 
+formatmenu = Menu(menubar, tearoff=0)
+
+textfontmenu = Menu(formatmenu, tearoff=0)
+textfontmenu.add_command(label="Arial")
+formatmenu.add_cascade(label="Font", menu=textfontmenu)
+
+textsizemenu = Menu(formatmenu, tearoff=0)
+textsizemenu.add_command(label="3")
+textsizemenu.add_command(label="Increase Font Size", accelerator="Alt+.", command=increase_font)
+root.bind_all("<Alt-.>", increase_font)
+textsizemenu.add_command(label="Decrease Font Size", accelerator="Alt+,", command=decrease_font)
+root.bind_all("<Alt-,>", decrease_font)
+formatmenu.add_cascade(label="Text Size", menu=textsizemenu)
+
+formatmenu.add_command(label="Text Colour", command=change_text_colour)
+
+textstylemenu = Menu(formatmenu, tearoff=0)
+textstylemenu.add_command(label="Normal", command=normal_text_style)
+textstylemenu.add_command(label="Bold", accelerator= "Ctrl+B", command=bold_text_style)
+root.bind_all("<Control-b>", bold_text_style)
+textstylemenu.add_command(label="Italic", command=italic_text_style)
+formatmenu.add_cascade(label="Text Style", menu=textstylemenu)
+
+menubar.add_cascade(label="Format", menu=formatmenu)
+
 helpmenu = Menu(menubar, tearoff=0)
-helpmenu.add_command(label="Documentation", accelerator="Ctrl+Shift+D", command=documentation)
-root.bind_all("<Control-Shift-d>", documentation)
-helpmenu.add_command(label="About", accelerator="Ctrl+Shift+A", command=about_menu)
-root.bind_all("<Control-Shift-a>", about_menu)
+helpmenu.add_command(label="About CTEIE", command=about_menu)
+helpmenu.add_command(label="Documentation", command=documentation)
 menubar.add_cascade(label="Help", menu=helpmenu)
 
 root.config(menu=menubar)
--- Text Editor Terminal Version.py
+++ Text Editor Terminal Version.py
@@ -1,10 +1,10 @@
 import os
 
 password = input("\nWhat is the password? ")
-if password == "":
+if password == "password":
     loop = True
-    if not os.path.exists("C:/Editor/Notes/"):
-        os.makedirs("C:/Editor/Notes/")
+    if not os.path.exists("C:/Users/2005s/Documents/Visual Studio Code/Python/Tkinter/Custom-Text-Editor/"):
+        os.makedirs("C:/Users/2005s/Documents/Visual Studio Code/Python/Tkinter/Custom-Text-Editor/")
 else:
     loop = False
 
@@ -12,17 +12,17 @@ while loop == True:
     task = input("\nWhat would like to do?    New    View    Write/Edit    Exit    : ")
     if task == "New":
         new_file_name = input("\nWhat is the name of the file to be created? ")
-        open(f"C:/Editor/Notes/{new_file_name}.ctetv", "w+")
+        open(f"C:/Users/2005s/Documents/Visual Studio Code/Python/Tkinter/Custom-Text-Editor/{new_file_name}.ctetv", "w+")
         print("\nCreated new note.")
     elif task == "View":
         open_file_name = input("\nWhat is the name of the file to be opened? ")
         try:
-            with open(f"C:/Editor/Notes/{open_file_name}.ctetv", "r") as f:
+            with open(f"C:/Users/2005s/Documents/Visual Studio Code/Python/Tkinter/Custom-Text-Editor/{open_file_name}.ctetv", "r") as f:
                 content = f.read()
                 lines = content.split(" /n ")
                 showing = "\n".join(lines)
 
-            print(f"\nViewing 'C:/Editor/Notes/{open_file_name}.ctetv'.")
+            print(f"\nViewing 'C:/Users/2005s/Documents/Visual Studio Code/Python/Tkinter/Custom-Text-Editor/{open_file_name}.ctetv'.")
             print("--------------------------------------------------")
             print(showing)
             print("--------------------------------------------------\n")
@@ -33,7 +33,7 @@ while loop == True:
         edit_file_name = input("\nWhat is the name of the file to be edited? ")
         written = input("Write what you want to save. Use '/n' to add a line break in between. \n\n")
         print("/n")
-        with open(f"C:/Editor/Notes/{edit_file_name}.ctetv", "w") as f:
+        with open(f"C:/Users/2005s/Documents/Visual Studio Code/Python/Tkinter/Custom-Text-Editor/{edit_file_name}.ctetv", "w") as f:
             f.write(written)
         print("\nSaved succesfully!")
     elif task == "Exit":
