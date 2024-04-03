7c25b804bd5fcd2083d41871433467b417e54c61
Sooraj Sannabhadti <2005sooraj@gmail.com>
2024-04-02 18:33:50 -0700

    Small fixes and v1.9.0 release

--- installer_creator.py
+++ installer_creator.py
@@ -3,7 +3,6 @@
 from os import rename, path, remove
 from shutil import rmtree
 import hashlib
-
 import PyInstaller.__main__
 
 version = "1.9.0"
@@ -61,7 +60,9 @@ with open("encryptext_installer.py", "r+") as file:
     file.truncate()
 
 # Move the exe out of the dist folder
-remove(f"encryptext_installer_v{version}_64bit.exe")
+try:
+    remove(f"encryptext_installer_v{version}_64bit.exe")
+except FileNotFoundError: pass
 rename(path.join("dist", "encryptext_installer.exe"), f"encryptext_installer_v{version}_64bit.exe")
 
 # Remove pyinstaller folders and files
